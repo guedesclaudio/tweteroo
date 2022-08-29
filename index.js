214f79803eecb2b57220d84a1e7f4868cac91fe5
guedesclaudio <cgsarmentosilva@gmail.com>
2022-08-29 13:46:35 -0300

    FIX : tweets visualization

--- index.js
+++ index.js
@@ -15,45 +15,28 @@ server.post("/sign-up", (req, res) => {
     if(!username || !avatar) {
         return res.status(400).send("Todos os campos são obrigatórios!")
     }
+    if(DB_USER.find(value => value.username === username)) {
+        return res.status(409).send("Já existe um usuário com esse nome")
+    }
+
     DB_USER.push({username, avatar})
     res.status(201).send("OK")
-    
 })
 
 server.post("/tweets", (req, res) => {
     const {tweet} = req.body
     const username = req.headers.user
+
     if(!username || !tweet) {
         return res.status(400).send("Todos os campos são obrigatórios!")
     }
+
     DB_TWEETS.push({username, tweet})
+    console.log(DB_TWEETS)
     res.status(201).send("OK")
 })
 
 server.get("/tweets", (req, res) => {
-
-    const page = Number(req.query.page)
-    const limitTweets = 10
-    let firstTweet = DB_TWEETS.length - (page * limitTweets)
-    let lastTweet = (page - 1) * limitTweets
-
-    if (DB_TWEETS.length <= limitTweets) {
-        firstTweet = 0
-    }
-    else {
-        firstTweet < 0 ? firstTweet = 0 : firstTweet = firstTweet
-    }
-    if (lastTweet > DB_TWEETS.length) {
-        lastTweet = 0
-    }
-    else {
-        firstTweet === 0 ? lastTweet = DB_TWEETS.length - lastTweet : lastTweet = firstTweet + limitTweets
-    }
-
-    if (page && (page <= 0 || typeof(page) !== "number")) {
-        return res.status(400).send("Informe uma página válida!")
-        
-    }
 
     if(DB_TWEETS.length > 0) {
         DB_TWEETS.forEach(tweet => {
@@ -61,16 +44,20 @@ server.get("/tweets", (req, res) => {
             tweet.avatar = user.avatar
         })
     }
-    /*const lastTweets = DB_TWEETS.length >= 10 ?
-    DB_TWEETS.slice(firstTweet, lastTweet) :
-    DB_TWEETS*/
-    console.log(firstTweet, lastTweet)
-    res.send(DB_TWEETS.slice(firstTweet, lastTweet))
+
+    const lastTweets = DB_TWEETS.length >= 10 ?
+    DB_TWEETS.slice(DB_TWEETS.length-10, DB_TWEETS.length) :
+    DB_TWEETS
+
+    lastTweets.reverse()
+    res.send(lastTweets)
+    lastTweets.reverse()
 })
 
 server.get("/tweets/:username", (req, res) => {
     const {username} = req.params
     const filtredTweets = DB_TWEETS.find(value => value.username === username)
+    
     res.send(filtredTweets)
 })
 
